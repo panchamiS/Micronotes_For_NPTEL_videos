1370f33ac8bdd6ad2f1412803a96691807767f14
sskanchan <surendra.skanchan@gmail.com>
2014-02-03 22:15:55 +0530

    added some structure

---
+++ view/scripts/layout_handler.js
@@ -0,0 +1,16 @@
+$(document).ready(handleLayout(event));
+
+function handleLayout(event)
+{
+	WIDTH = window.innerWidth;
+	HEIGHT = window.innerHeight;
+
+	body_wrapper = $("#wrapper");
+	head_space = $("#head_space");
+	container = $("#container");
+	head_nav_bar = $("#head_nav_bar");
+	video_container = $("#video_container");
+	video = $("#video");
+	footer_nav_bar = $("#footer_nav_bar");
+	//console.log(WIDTH);
+}
